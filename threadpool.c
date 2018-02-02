15db749ba17775156e4efb5a14e2aa7a9e6ce52f
Alex J. Malozemoff <amaloz@galois.com>
2018-02-02 10:33:24 -0800

    try to fix deadlock

--- threadpool.c
+++ threadpool.c
@@ -34,12 +34,12 @@ threadpool_create(size_t nthreads)
 
     if ((pool = calloc(1, sizeof pool[0])) == NULL)
         return NULL;
-
     pool->nthreads = nthreads;
     if ((pool->threads = calloc(nthreads, sizeof pool->threads[0])) == NULL)
         goto error;
 
     atomic_init(&pool->num_active_jobs, 0);
+    atomic_init(&pool->flag_exit_please, false);
     pool->first_job = NULL;
     pool->last_job = NULL;
 
@@ -47,7 +47,6 @@ threadpool_create(size_t nthreads)
     pthread_mutex_init(&pool->worker_wakeup_lock, NULL);
     pthread_cond_init(&pool->worker_wakeup_cond, NULL);
 
-    atomic_init(&pool->flag_exit_please, false);
     for (size_t i = 0; i < nthreads; i++)
         (void) pthread_create(&pool->threads[i], NULL, threadpool_worker, pool);
     return pool;
@@ -62,9 +61,7 @@ threadpool_destroy(threadpool *pool)
 {
     if (pool == NULL)
         return;
-    pthread_mutex_lock(&pool->job_list_lock);
     atomic_store(&pool->flag_exit_please, true);
-    pthread_mutex_unlock(&pool->job_list_lock);
     pthread_cond_broadcast(&pool->worker_wakeup_cond);
     threadpool_wait(pool);
 
@@ -108,8 +105,10 @@ threadpool_add_job(threadpool *pool, void (*func)(void *), void *arg)
 void
 threadpool_wait(threadpool *pool)
 {
+    const struct timespec t = { .tv_sec = 0, .tv_nsec = 1000000L };
     while (atomic_load(&pool->num_active_jobs) > 0) {
         pthread_cond_broadcast(&pool->worker_wakeup_cond);
+        (void) nanosleep(&t, NULL);
     }
 }
 
@@ -143,13 +142,7 @@ threadpool_worker(void *pool_)
             if (atomic_load(&pool->flag_exit_please))
                 pthread_exit(NULL);
             pthread_mutex_lock(&pool->worker_wakeup_lock);
-            pthread_mutex_lock(&pool->job_list_lock);
-            if (job_available(pool) || atomic_load(&pool->flag_exit_please)) {
-                pthread_mutex_unlock(&pool->job_list_lock);
-            } else {
-                pthread_mutex_unlock(&pool->job_list_lock);
-                pthread_cond_wait(&pool->worker_wakeup_cond, &pool->worker_wakeup_lock);
-            }
+            pthread_cond_wait(&pool->worker_wakeup_cond, &pool->worker_wakeup_lock);
             pthread_mutex_unlock(&pool->worker_wakeup_lock);
         }
     }
--- threadpool.h
+++ threadpool.h
@@ -9,8 +9,8 @@
 typedef struct threadpool threadpool;
 
 threadpool * threadpool_create(size_t nthreads);
-void threadpool_destroy(threadpool *pool);
-int threadpool_add_job(threadpool *pool, void (*func)(void *), void *arg);
-void threadpool_wait(threadpool *pool);
+void         threadpool_destroy(threadpool *pool);
+int          threadpool_add_job(threadpool *pool, void (*func)(void *), void *arg);
+void         threadpool_wait(threadpool *pool);
 
 #endif
