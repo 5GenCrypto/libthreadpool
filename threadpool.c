3c7070e4702f3764086ec300ca71e28530601939
Alex J. Malozemoff <amaloz@galois.com>
2018-02-13 12:35:21 -0800

    agressive lock management --- hopefully this doesn't break things!

--- threadpool.c
+++ threadpool.c
@@ -1,11 +1,10 @@
 #include "threadpool.h"
 
-#include <assert.h>
 #include <pthread.h>
 #include <stdatomic.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct job_t {
     void (*func)(void *);
@@ -13,15 +12,19 @@ typedef struct job_t {
     struct job_t *next;
 } job_t;
 
+typedef struct {
+    threadpool *pool;
+    size_t idx;
+} args_t;
+
 struct threadpool {
     size_t nthreads;
     pthread_t *threads;
-    atomic_size_t num_active_jobs;
-    job_t *first_job;
-    job_t *last_job;
-    pthread_mutex_t job_list_lock;
-    pthread_mutex_t worker_wakeup_lock;
-    pthread_cond_t  worker_wakeup_cond;
+    job_t *first, *last;
+    pthread_mutex_t job_lock;
+    pthread_mutex_t *wakeup_locks;
+    args_t *args;
+    pthread_cond_t  wakeup_cond;
     atomic_bool flag_exit_please;
 };
 
@@ -38,17 +41,20 @@ threadpool_create(size_t nthreads)
     if ((pool->threads = calloc(nthreads, sizeof pool->threads[0])) == NULL)
         goto error;
 
-    atomic_init(&pool->num_active_jobs, 0);
     atomic_init(&pool->flag_exit_please, false);
-    pool->first_job = NULL;
-    pool->last_job = NULL;
+    pool->first = NULL;
+    pool->last = NULL;
 
-    pthread_mutex_init(&pool->job_list_lock, NULL);
-    pthread_mutex_init(&pool->worker_wakeup_lock, NULL);
-    pthread_cond_init(&pool->worker_wakeup_cond, NULL);
-
-    for (size_t i = 0; i < nthreads; i++)
-        (void) pthread_create(&pool->threads[i], NULL, threadpool_worker, pool);
+    pthread_mutex_init(&pool->job_lock, NULL);
+    pthread_cond_init(&pool->wakeup_cond, NULL);
+    pool->wakeup_locks = calloc(nthreads, sizeof pool->wakeup_locks[0]);
+    pool->args = calloc(nthreads, sizeof pool->args[0]);
+    for (size_t i = 0; i < nthreads; i++) {
+        pool->args[i].pool = pool;
+        pool->args[i].idx = i;
+        pthread_mutex_init(&pool->wakeup_locks[i], NULL);
+        (void) pthread_create(&pool->threads[i], NULL, threadpool_worker, &pool->args[i]);
+    }
     return pool;
 error:
     if (pool)
@@ -59,19 +65,19 @@ error:
 void
 threadpool_destroy(threadpool *pool)
 {
-    if (pool == NULL)
-        return;
+    if (pool == NULL) return;
     atomic_store(&pool->flag_exit_please, true);
-    pthread_cond_broadcast(&pool->worker_wakeup_cond);
-    threadpool_wait(pool);
-
+    pthread_cond_broadcast(&pool->wakeup_cond);
     for (size_t i = 0; i < pool->nthreads; i++)
         (void) pthread_join(pool->threads[i], NULL);
     free(pool->threads);
 
-    pthread_mutex_destroy(&pool->job_list_lock);
-    pthread_mutex_destroy(&pool->worker_wakeup_lock);
-    pthread_cond_destroy(&pool->worker_wakeup_cond);
+    pthread_mutex_destroy(&pool->job_lock);
+    for (size_t i = 0; i < pool->nthreads; i++)
+        pthread_mutex_destroy(&pool->wakeup_locks[i]);
+    free(pool->wakeup_locks);
+    free(pool->args);
+    pthread_cond_destroy(&pool->wakeup_cond);
 
     free(pool);
 }
@@ -79,7 +85,7 @@ threadpool_destroy(threadpool *pool)
 int
 threadpool_add_job(threadpool *pool, void (*func)(void *), void *arg)
 {
-    job_t *new, *tmp;
+    job_t *new;
 
     if ((new = calloc(1, sizeof new[0])) == NULL)
         return THREADPOOL_ERR;
@@ -87,65 +93,51 @@ threadpool_add_job(threadpool *pool, void (*func)(void *), void *arg)
     new->arg  = arg;
     new->next = NULL;
 
-    pthread_mutex_lock(&pool->job_list_lock);
+    pthread_mutex_lock(&pool->job_lock);
     {
-        if (pool->first_job == NULL) {
-            pool->first_job = pool->last_job = new;
+        if (pool->first) {
+            job_t *tmp = pool->last;
+            pool->last = tmp->next = new;
         } else {
-            tmp = pool->last_job;
-            pool->last_job = tmp->next = new;
+            pool->first = pool->last = new;
         }
-        atomic_fetch_add(&pool->num_active_jobs, 1);
     }
-    pthread_mutex_unlock(&pool->job_list_lock);
-    pthread_cond_signal(&pool->worker_wakeup_cond);
+    pthread_mutex_unlock(&pool->job_lock);
+    pthread_cond_signal(&pool->wakeup_cond);
     return THREADPOOL_OK;
 }
 
-void
-threadpool_wait(threadpool *pool)
-{
-    const struct timespec t = { .tv_sec = 0, .tv_nsec = 1000000L };
-    while (atomic_load(&pool->num_active_jobs) > 0) {
-        pthread_cond_broadcast(&pool->worker_wakeup_cond);
-        (void) nanosleep(&t, NULL);
-    }
-}
-
-static bool
-job_available(const threadpool *pool)
-{
-    /* assumes we have the pool->job_list_lock lock */
-    return pool->first_job != NULL;
-}
-
 static void *
-threadpool_worker(void *pool_)
+threadpool_worker(void *args_)
 {
-    const struct timespec t = { .tv_sec = 0, .tv_nsec = 1000000L };
-    threadpool *pool = pool_;
+    const struct timespec t = { .tv_sec = 0, .tv_nsec = 100000L };
+
+    args_t *args = args_;
+    threadpool *pool = args->pool;
+    size_t idx = args->idx;
     while (1) {
-        pthread_mutex_lock(&pool->job_list_lock);
-        if (job_available(pool)) {
-            job_t *node;
-            node = pool->first_job;
-            pool->first_job = node->next;
-            if (pool->first_job == NULL)
-                pool->last_job = NULL;
-            pthread_mutex_unlock(&pool->job_list_lock);
+        if (pool->first) {
+            job_t *node = NULL;
+            pthread_mutex_lock(&pool->job_lock);
+            if (pool->first) {
+                node = pool->first;
+                pool->first = node->next;
+                if (pool->first == NULL)
+                    pool->last = NULL;
+            }
+            pthread_mutex_unlock(&pool->job_lock);
 
-            node->func(node->arg);
-            free(node);
-
-            atomic_fetch_sub(&pool->num_active_jobs, 1);
+            if (node) {
+                node->func(node->arg);
+                free(node);
+            }
         } else {
-            pthread_mutex_unlock(&pool->job_list_lock);
             if (atomic_load(&pool->flag_exit_please))
                 pthread_exit(NULL);
-            pthread_mutex_lock(&pool->worker_wakeup_lock);
-            pthread_cond_timedwait(&pool->worker_wakeup_cond,
-                                   &pool->worker_wakeup_lock, &t);
-            pthread_mutex_unlock(&pool->worker_wakeup_lock);
+            pthread_mutex_lock(&pool->wakeup_locks[idx]);
+            pthread_cond_timedwait(&pool->wakeup_cond,
+                                   &pool->wakeup_locks[idx], &t);
+            pthread_mutex_unlock(&pool->wakeup_locks[idx]);
         }
     }
 }
